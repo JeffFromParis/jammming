893cfccd7f4709e742c2aaca073dc3b4ccbcc75f
Jeff Vergne <vergnejf@gmail.com>
2022-02-18 10:59:14 +0100

    Step 22 to 28 - create Track and TrackList Components

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,7 +4,13 @@ import './TrackList.css';
 class TrackList extends React.Component{
     render(){
         return(
-
+            <div className="TrackList">
+                <ul>
+                    <li>Song 1</li>
+                    <li>Song 2</li>
+                    <li>Song 3</li>
+                </ul>
+            </div>
         );
     }
 }
